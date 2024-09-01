43f8bfd7bb90854d24da0f4639f70ef80e389202
sajakshrestha8 <shresthasajak123@gmail.com>
2024-09-01 21:13:16 +0545

    div started moving but moves outside the viewport tooo

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-let box1 = document.getElementById("box1");
 let box2 = document.getElementById("box2");
+let box1 = document.getElementById("box1");
 
-box1.style.left = "20px";
+var h = window.innerHeight - 50;
+
+console.log(h);
+
+setInterval(() => {
+  box1.style.left = box1.offsetLeft + "px";
+  //   box1.style.top = box1.offsetTop + 1 + "px";
+  //   box2
+
+  if (box1.offsetLeft + box1.offsetWidth >= window.innerWidth) {
+    box1.style.top = box1.offsetTop + "px";
+    box1.style.left = "none";
+  }
+}, 100);
+
+console.log("running");
+console.log(box1.offsetLeft);
