44aaa4943b577e4b257f0cee03fd0885e483dd0c
sajakshrestha8 <shresthasajak123@gmail.com>
2024-09-02 21:13:43 +0545

    final Update

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ var h = window.innerHeight - 50;
 
 console.log(h);
 
-setInterval(() => {
-  box1.style.left = box1.offsetLeft + "px";
-  //   box1.style.top = box1.offsetTop + 1 + "px";
-  //   box2
+let viewWidth = window.innerWidth - 200;
+let viewHeight = window.innerHeight - 200;
+console.log(viewWidth);
+console.log(viewWidth);
 
-  if (box1.offsetLeft + box1.offsetWidth >= window.innerWidth) {
-    box1.style.top = box1.offsetTop + "px";
-    box1.style.left = "none";
-  }
-}, 100);
+setInterval(() => {
+  //box 1
+  box1.style.left = Math.floor(Math.random() * viewWidth) + "px";
+  box1.style.top = Math.floor(Math.random() * viewHeight) + "px";
+  // console.log(Math.floor(Math.random() * 1000));
+
+  //box 2
+  box2.style.left = Math.floor(Math.random() * viewWidth) + "px";
+  box2.style.top = Math.floor(Math.random() * viewHeight) + "px";
+}, 500);
 
 console.log("running");
 console.log(box1.offsetLeft);
